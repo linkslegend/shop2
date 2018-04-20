1eaaacdb9c01498c9c7e62c3a82d2c6149dbf2bd
linkslegend <rd.intermac@googlemail.com>
2018-04-20 20:19:22 +0200

    Minor changes
    - changed logo to svg
    - image zoom in cursor is now svg
    - many small fixes

--- functions.php
+++ functions.php
@@ -355,8 +355,8 @@ function wooc_extra_register_fields() {?>
 // Remove WooCommerce core style one by one
  add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
  function jk_dequeue_styles( $enqueue_styles ) {
- 	//unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
- 	//unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+ 	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+ 	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
  	unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
  	return $enqueue_styles;
  }
